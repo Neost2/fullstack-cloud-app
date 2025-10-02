c2fb544c442af1921a50061c536f0f6f78228f03
Neost <neost2@hotmail.com>
2025-10-02 14:47:54 -0500

    as

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
-
 const bucket = new aws.s3.Bucket("react-app-bucket1", {
   website: {
     indexDocument: "index.html, error.html",
@@ -16,9 +15,19 @@ new aws.s3.BucketPolicy("bucketPolicy", {
       Version: "2012-10-17",
       Statement: [
         {
+          Sid: "PublicReadGetObject",
           Effect: "Allow",
           Principal: "*",
           Action: ["s3:GetObject"],
+          Resource: [`arn:aws:s3:::${id}/*`],
+        },
+        {
+          Sid: "AllowPutObject",
+          Effect: "Allow",
+          Principal: {
+            AWS: "arn:aws:iam::123456789012:user/Admin",
+          },
+          Action: ["s3:PutObject", "s3:PutObjectAcl"],
           Resource: [`arn:aws:s3:::${id}/*`],
         },
       ],
